0cb1f0873b83b208b7e79e7d96236f5a78d91a58
Javier Castillo <javiercame98@gmail.com>
2026-05-06 10:32:15 +0200

    feat: innitial commit

---
+++ trends-api/src/main/java/com/culturaltrends/api/TrendsApiApplication.java
@@ -0,0 +1,13 @@
+package com.culturaltrends.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrendsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrendsApiApplication.class, args);
+	}
+
+}
---
+++ trends-api/src/test/java/com/culturaltrends/api/TrendsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.culturaltrends.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrendsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
